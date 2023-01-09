1e88c4ceaa4feefe87edcdfe8dd6a8f854a20dab
Phạm Văn Đạt <phamvandat130402@gmail.com>
2023-01-09 22:39:53 +0700

    update interface

---
+++ OOPDemo/app/src/main/java/com/datpv134/oopdemo/CarController.kt
@@ -0,0 +1,12 @@
+package com.datpv134.oopdemo
+
+interface CarController {
+
+    fun increaseSpeed()
+
+    fun decreaseSpeed()
+
+    fun getBrand():String {
+        return "Flutter"
+    }
+}
--- OOPDemo/app/src/main/java/com/datpv134/oopdemo/MyCar.kt
+++ OOPDemo/app/src/main/java/com/datpv134/oopdemo/MyCar.kt
@@ -2,9 +2,17 @@ package com.datpv134.oopdemo
 
 import android.util.Log
 
-class MyCar : Car() {
+class MyCar : Car(), CarController {
     override fun start() {
         super.start()
-        Log.i("MyTag", "This is my car starting...")
+        Log.i("MyTag", "This is my car starting... It's brand is ${getBrand()}")
+    }
+
+    override fun increaseSpeed() {
+
+    }
+
+    override fun decreaseSpeed() {
+
     }
 }
