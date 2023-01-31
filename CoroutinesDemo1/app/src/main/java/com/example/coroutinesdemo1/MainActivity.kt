d097b0ca3b3cc1ce2e2f2e9415b9965ed0cfddd5
Phạm Văn Đạt <phamvandat130402@gmail.com>
2023-01-31 14:45:17 +0700

    change coroutine thread

--- CoroutinesDemo1/app/src/main/java/com/example/coroutinesdemo1/MainActivity.kt
+++ CoroutinesDemo1/app/src/main/java/com/example/coroutinesdemo1/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.example.coroutinesdemo1
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     private var count = 0
+    private lateinit var tvMessage: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +19,7 @@ class MainActivity : AppCompatActivity() {
         val textView = findViewById<TextView>(R.id.tvCount)
         val countButton = findViewById<Button>(R.id.btnCount)
         val downloadButton = findViewById<Button>(R.id.btnDownload)
+        tvMessage = findViewById(R.id.tvMessage)
 
         countButton.setOnClickListener {
             textView.text = count++.toString()
@@ -34,6 +34,9 @@ class MainActivity : AppCompatActivity() {
     private suspend fun downloadUserData() {
         for (i in 1..200000) {
             Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+            withContext(Dispatchers.Main) {
+                tvMessage.text = "Downloading user $i"
+            }
             delay(50)
         }
     }
