1507f38bf198147d61d6642d954750c2ec7ede9f
Phạm Văn Đạt <phamvandat130402@gmail.com>
2023-02-22 15:18:52 +0700

    get data from rest api

--- RetrofitDemo/app/src/main/java/com/example/retrofitdemo/MainActivity.kt
+++ RetrofitDemo/app/src/main/java/com/example/retrofitdemo/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.retrofitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.liveData
+import com.example.retrofitdemo.databinding.ActivityMainBinding
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val restService = RetrofitInstance.getRetrofitInstance().create(AlbumService::class.java)
+
+        val responseLiveData: LiveData<Response<Album>> = liveData {
+            val response = restService.getAlbum()
+            emit(response)
+        }
+
+        responseLiveData.observe(this, Observer {
+            val albumList = it.body()?.listIterator()
+            if (albumList != null) {
+                while (albumList.hasNext()) {
+                    val albumItem = albumList.next()
+                    val strAlbum = "${albumItem.userId}" + "\n" + " ${albumItem.id}" + "\n" + " ${albumItem.title}" + "\n\n"
+                    binding.tvAlbums.append(strAlbum)
+                }
+            }
+        })
     }
 }
