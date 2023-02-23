3d978da60da81bd4a41f4926c4f317a46e22bfdf
Phạm Văn Đạt <phamvandat130402@gmail.com>
2023-02-23 08:31:04 +0700

    use query parameter to get data by userId

--- RetrofitDemo/app/src/main/java/com/example/retrofitdemo/AlbumService.kt
+++ RetrofitDemo/app/src/main/java/com/example/retrofitdemo/AlbumService.kt
@@ -2,8 +2,11 @@ package com.example.retrofitdemo
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface AlbumService {
     @GET("albums")
     suspend fun getAlbum(): Response<Album>
+    @GET("albums")
+    suspend fun getAlbumByUserId(@Query("userId") userId: Int): Response<Album>
 }
--- RetrofitDemo/app/src/main/java/com/example/retrofitdemo/MainActivity.kt
+++ RetrofitDemo/app/src/main/java/com/example/retrofitdemo/MainActivity.kt
@@ -16,6 +16,40 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.apply {
+            btnGetAll.setOnClickListener {
+                getAllAlbum()
+            }
+
+            btnGetByUser.setOnClickListener {
+                getAlbumByUserId(3)
+            }
+        }
+    }
+
+    private fun getAlbumByUserId(i: Int) {
+        binding.tvAlbums.text = ""
+        val restService = RetrofitInstance.getRetrofitInstance().create(AlbumService::class.java)
+
+        val responseLiveData: LiveData<Response<Album>> = liveData {
+            val response = restService.getAlbumByUserId(i)
+            emit(response)
+        }
+
+        responseLiveData.observe(this, Observer {
+            val albumList = it.body()?.listIterator()
+            if (albumList != null) {
+                while (albumList.hasNext()) {
+                    val albumItem = albumList.next()
+                    val strAlbum = "User ID: ${albumItem.userId}" + "\n" + "ID: ${albumItem.id}" + "\n" + "Title: ${albumItem.title}" + "\n\n"
+                    binding.tvAlbums.append(strAlbum)
+                }
+            }
+        })
+    }
+
+    private fun getAllAlbum() {
+        binding.tvAlbums.text = ""
         val restService = RetrofitInstance.getRetrofitInstance().create(AlbumService::class.java)
 
         val responseLiveData: LiveData<Response<Album>> = liveData {
@@ -28,7 +62,7 @@ class MainActivity : AppCompatActivity() {
             if (albumList != null) {
                 while (albumList.hasNext()) {
                     val albumItem = albumList.next()
-                    val strAlbum = "${albumItem.userId}" + "\n" + " ${albumItem.id}" + "\n" + " ${albumItem.title}" + "\n\n"
+                    val strAlbum = "User ID: ${albumItem.userId}" + "\n" + "ID: ${albumItem.id}" + "\n" + "Title: ${albumItem.title}" + "\n\n"
                     binding.tvAlbums.append(strAlbum)
                 }
             }
