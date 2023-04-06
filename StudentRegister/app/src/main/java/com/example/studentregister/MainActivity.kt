01bf3b93761d863757ee8652f5ff1c172fa57cde
Phạm Văn Đạt <phamvandat130402@gmail.com>
2023-04-06 14:39:14 +0700

    Canvas Demo

--- StudentRegister/app/src/main/java/com/example/studentregister/MainActivity.kt
+++ StudentRegister/app/src/main/java/com/example/studentregister/MainActivity.kt
@@ -48,7 +48,6 @@ class MainActivity : AppCompatActivity() {
 
         initRecyclerView()
     }
-
     private fun saveStudentData() {
         binding.apply {
             viewModel.insertStudent(
